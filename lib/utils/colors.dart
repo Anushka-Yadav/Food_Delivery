cd0e809c0d824360186b1397b5a07b01dd71483b
Anushka-Yadav <lncbbtcse026@lnctu.ac.in>
2023-10-27 08:19:38 +0530

    blur color added

--- lib/utils/colors.dart
+++ lib/utils/colors.dart
@@ -11,5 +11,6 @@ class AppColors{
   static final Color titleColor = const Color(0xFF5c524f);
   static final Color mainBlackColor = const Color(0xFF332d2b);
   static final Color yellowColor = const Color(0xffe7c66d);
+  static final Color bluecolor = const Color(0xff5388e0);
 
 }
